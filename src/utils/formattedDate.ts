d9b0299a8e9b95a452474ac26ff22d0bdbcf3876
adib <adibhaidarzaky@gmail.com>
2025-02-03 10:23:51 +0700

    add formattedShortFullDate function

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -10,3 +10,11 @@ export const formattedShortDate = (date: string) => {
     const formatted = new Date(date).toLocaleDateString("id-ID", { day: "2-digit", month: "short" });
     return formatted;
 }
+
+export const formattedShortFullDate = (date: string) => {
+    return new Date(date).toLocaleDateString("id-ID", {
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+}
